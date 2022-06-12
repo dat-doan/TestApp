e1fe0f4a6fc78712fa0be9dbceec69ff3deba54e
Dat N. Doan <Dat.NgocDoan@coxautoinc.com>
2022-06-12 16:48:30 +0700

    Initial Commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by DatDN4.BU91 on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by DatDN4.BU91 on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
